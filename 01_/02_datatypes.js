48134d461495e312ddbdc373e440c467bfb6c5a9
AsimaArifTahir <asimaariftahir@gmail.com>
2024-07-08 08:41:00 +0000

    Date Type

---
+++ 01_/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // use all JS code as newer version
+//alert(3+3) //we are using nodeJS not browser
+console.log(3+3); 
+console.log("Asima");//code should be readable and future proof, code readability should be high
+
+//Date Type
+
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => yes or no, true or false
+// undefind => undefined
+// null => stand alone value, data type of null in object(will read this in future)
+// symbol => unique value
+
+//object type
