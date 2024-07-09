ee7ce822b6cfce4793f71954e3270ebed88ee941
AsimaArifTahir <asimaariftahir@gmail.com>
2024-07-09 07:00:52 +0000

    Data conversions

--- 01_/03_conversionOperations.js
+++ 01_/03_conversionOperations.js
@@ -1,3 +1,5 @@
+// CONVERSIONS
+
 let score = "hitesh"
 
 //console.log(typeof score);
@@ -26,3 +28,8 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// OPERATIONS
+let value = 3
+let negValue = -value
+console.log(negValue);
